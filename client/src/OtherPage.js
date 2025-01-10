6ce2a15bbcaa58d4f88da24c8868e82dd0b3061c
rone <rone@ennui.org>
2025-01-09 16:31:06 -0800

    Commit starter code

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      I'm a big dummy!
+      <Link to="/">Get outta here</Link>
+    </div>
+  );
+};
+export default OtherPage;
